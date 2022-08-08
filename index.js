4aae11942f38eae0707b3e7b57f2a1a91a7addd5
Andrey Dmitriev <admit8490@gmail.com>
2022-08-09 01:43:44 +0200

    removed: extra comments, fixed: indentation

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-//CALCULATIONS
-
-//UNIT1
-
 const hexToRGB = (hexColor) => {
     const R = parseInt(hexColor.slice(1, 3), 16);
     const G = parseInt(hexColor.slice(3, 5), 16);
     const B = parseInt(hexColor.slice(5), 16);
     return [R, G, B];
   }
-
-//UNIT2
   
 const getLuminance = (RGBarray) => {
     //convert 8bit colors to
     //RsRGB, GsRGB BsRGB
-    const XsRGBarray = RGBarray.map(elem => elem / 255);
-
-    //use function to get r, g, b for final formula
-    //UNIT2.1
+    const XsRGBarray = RGBarray.map(elem => elem / 255);    
+    
     const getRGBCoefficient = (XsRGB) => {
-    if (XsRGB <= 0.03928) {
-        return XsRGB / 12.92;
-    }
-    else {
-        return Math.pow((XsRGB + 0.055) / 1.055, 2.4);
-    }
+      if (XsRGB <= 0.03928) {
+          return XsRGB / 12.92;
+      }
+      else {
+          return Math.pow((XsRGB + 0.055) / 1.055, 2.4);
+      }
     }
 
     //array of multipliers, for r, g, b respectively
@@ -40,18 +32,14 @@ const getLuminance = (RGBarray) => {
     return luminance;
 }
 
-const twoHexesRatio = (color1, color2) => {
-    //STEP1 convert hex into rgb using UNIT1
+const twoHexesRatio = (color1, color2) => {    
     const RGBColor1 = hexToRGB(color1);
-    const RGBColor2 = hexToRGB(color2);
-    //STEP2 get relative luminance for each of colors using UNIT2
+    const RGBColor2 = hexToRGB(color2);    
     const color1Luminance = getLuminance(RGBColor1);
-    const color2Luminance = getLuminance(RGBColor2);
-    /*STEP3 calculate result value
-    (L1 + 0.05) / (L2 + 0.05), whereby:
+    const color2Luminance = getLuminance(RGBColor2);    
+    /* (L1 + 0.05) / (L2 + 0.05), whereby:
     L1 is the relative luminance of the lighter of the colors, and
-    L2 is the relative luminance of the darker of the colors.
-    */
+    L2 is the relative luminance of the darker of the colors. */    
     let [lighterLum, darkerLum] = [color1Luminance, color2Luminance];
     if (darkerLum > lighterLum) {
       [lighterLum, darkerLum] = [darkerLum, lighterLum];
@@ -59,14 +47,9 @@ const twoHexesRatio = (color1, color2) => {
     return ((lighterLum + 0.05) / (darkerLum + 0.05)).toFixed(2);
 }
 
-
-//HTML JS INTERACTION
-
 const giveRatio = () => {
     let firstColor = document.getElementById("foreground-color").value;
-    let secondColor = document.getElementById("background-color").value;
-    //CHECK for valid input
-    //for now I am just checking length
+    let secondColor = document.getElementById("background-color").value;    
     if (firstColor.length === 7 &&
         secondColor.length === 7 ){
       document.getElementById("contrast-ratio-result").innerHTML = twoHexesRatio(firstColor, secondColor);
