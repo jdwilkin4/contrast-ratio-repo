1a51a24ba7d754fed1bb19f6d44976b70ffc8526
sparkO7 <108756150+sparkO7@users.noreply.github.com>
2022-08-16 18:08:55 +0100

    Feat: implemented rgb and rgba functionality (#71)
    * Implemented RGB and RGBA functionality
    
    * Added comments
    
    * Fixed requested changes

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const hexToRGB = (hexColor) => {
   return [R, G, B];
 };
 
+const rgbInputToRGBNumbers = (rgbColor) => {
+  [R, G, B] = rgbColor
+    .replace(/[^\d,]/g, "")
+    .split(",")
+    .map((RGBValue) => parseInt(RGBValue));
+  return [R, G, B];
+};
+
+// Determine the equivalent opaque RGB color
+// for a given partially transparent RGB color against a white background
+const rgbaToCloseRGB = (rgbaColor) => {
+  const bg = [255, 255, 255];
+  [R, G, B, o] = rgbaColor
+    .replace(/[^\d,.]/g, "")
+    .split(",")
+    .map((el) => parseFloat(el));
+  // Y = p * T + (1 - p) * B , where:
+  // p is the opacity of the top layer
+  // T is the rgb number of the top layer color
+  // B is the rgb number of the fully opaque bottom layer
+  // Y is the rgb number of the equivalent fully opaque color
+  return [R, G, B].map((colFg, idx) =>
+    Math.ceil(o * colFg + (1 - o) * bg[idx])
+  );
+};
+
 const getLuminance = (RGBarray) => {
   //convert 8bit colors to
   //RsRGB, GsRGB BsRGB
@@ -37,32 +63,61 @@ const getLuminance = (RGBarray) => {
   return luminance;
 };
 
-const twoHexesRatio = (color1, color2) => {
-  const RGBColor1 = hexToRGB(color1);
-  const RGBColor2 = hexToRGB(color2);
-  const color1Luminance = getLuminance(RGBColor1);
-  const color2Luminance = getLuminance(RGBColor2);
+const calculateRatio = (color1, color2) => {
+  const color1Luminance = getLuminance(color1);
+  const color2Luminance = getLuminance(color2);
   /* (L1 + 0.05) / (L2 + 0.05), whereby:
     L1 is the relative luminance of the lighter of the colors, and
     L2 is the relative luminance of the darker of the colors. */
-  let [lighterLum, darkerLum] = [color1Luminance, color2Luminance];
-  if (darkerLum > lighterLum) {
-    [lighterLum, darkerLum] = [darkerLum, lighterLum];
-  }
+  let lighterLum = Math.max(color1Luminance, color2Luminance);
+  let darkerLum = Math.min(color1Luminance, color2Luminance);
   return ((lighterLum + 0.05) / (darkerLum + 0.05)).toFixed(2);
+};
+
+const colorFormatRatio = (color1, color2, convertRatio) => {
+  const RGBColor1 = convertRatio(color1);
+  const RGBColor2 = convertRatio(color2);
+  return calculateRatio(RGBColor1, RGBColor2);
 };
 
 const displayResult = () => {
   let firstColor = foregroundColor.value;
   let secondColor = backgroundColor.value;
-  if (firstColor.length === 7) {
+  const rgbRegex = /^rgb.*/i;
+  const rgbaRegex = /^rgba.*/i;
+  if (
+    firstColor.length === 7 ||
+    rgbRegex.test(firstColor) ||
+    rgbaRegex.test(firstColor)
+  ) {
     foregroundSwatch.style.backgroundColor = firstColor;
   }
-  if (secondColor.length === 7) {
+  if (
+    secondColor.length === 7 ||
+    rgbRegex.test(secondColor) ||
+    rgbaRegex.test(secondColor)
+  ) {
     backgroundSwatch.style.backgroundColor = secondColor;
   }
+  // CASE two Hexes
   if (firstColor.length === 7 && secondColor.length === 7) {
-    ratioResult.innerHTML = twoHexesRatio(firstColor, secondColor);
+    ratioResult.innerHTML = colorFormatRatio(firstColor, secondColor, hexToRGB);
+  }
+  // CASE two RGBAs
+  else if (rgbaRegex.test(firstColor) && rgbaRegex.test(secondColor)) {
+    ratioResult.innerHTML = colorFormatRatio(
+      firstColor,
+      secondColor,
+      rgbaToCloseRGB
+    );
+  }
+  // CASE two RGBs
+  else if (rgbRegex.test(firstColor) && rgbRegex.test(secondColor)) {
+    ratioResult.innerHTML = colorFormatRatio(
+      firstColor,
+      secondColor,
+      rgbInputToRGBNumbers
+    );
   }
 };
 
